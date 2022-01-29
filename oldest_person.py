e533320a8597a12756208fdb28f31f982199d91a
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-29 19:05:42 +0900

    6

--- oldest_person.py
+++ oldest_person.py
@@ -1,3 +1,7 @@
+from operator import le
+import re
+
+
 def oldest(people:dict):
     """
     Given a dictionary containing the names and ages of a group of people, return the name of the oldest person.
@@ -6,4 +10,25 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    a=[]
+    for x in people.values():
+        a.append(x)
+    
+    largest=a[0]
+
+    for y in a:
+            if y>largest:
+                largest=y
+    for i,j in people.items():
+        if j==largest:
+            return str(i)
+
+
+
+
+
+        
+
+
+
+print(oldest({"Javohir": 22, "Sharof": 23, "Tolib": 34, "Rustam": 16}))
